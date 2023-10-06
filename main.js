812c5b332c1fb345b13af5203d25d8e6807d725c
PardaboyevKarim95 <pardaboyevk0@gmail.com>
2023-10-06 16:10:44 +0500

    add

--- main.js
+++
@@ -1,5 +0,0 @@
-var elButton = document.querySelector(".site-header__buttontou");
-let elnav = document.querySelector("nav");
-elButton.addEventListener("click", function () {
-  elnav.classList.toggle("dark");
-});
